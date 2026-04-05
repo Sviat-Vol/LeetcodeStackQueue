6845ea2154c2cbbdcf3e0358ac7daababaf3cca5
Sviatoslav Volianiuk <102855447+Sviat-Vol@users.noreply.github.com>
2026-04-05 12:11:03 +0300

    Implement Maximum_Frequency_Stack.py
    Розв'язано проблему використвуючи сливники, бо при спробі використання deque не проходило обмеження по часу.

---
+++ Maximum Frequency Stack/Maximum_Frequency_Stack.py
@@ -0,0 +1,21 @@
+class FreqStack:
+    def __init__(self):
+        self.freq = {}
+        self.group = {}
+        self.max_freq = 0
+
+    def push(self, val: int) -> None:
+        f = self.freq.get(val, 0) + 1
+        self.freq[val] = f
+        if f not in self.group:
+            self.group[f] = []
+        self.group[f].append(val)
+        if f > self.max_freq:
+            self.max_freq = f
+
+    def pop(self) -> int:
+        val = self.group[self.max_freq].pop()
+        self.freq[val] -= 1
+        if not self.group[self.max_freq]:
+            self.max_freq -= 1
+        return val
